1485399c18f057b4acfa2c8040213e2d0cd5f107
AlejandroMirallesRuiz <mirall271828s@gmail.com>
2025-02-26 12:26:06 +0100

    Type animation fixed

--- portfolio/src/components/Hero.vue
+++ portfolio/src/components/Hero.vue
@@ -1,4 +1,16 @@
 <script setup>
+import { Typed } from "@duskmoon/vue3-typed-js";
+
+const positions = ["Web Developer", "Full-Stack Developer", "Back-End Developer", "Developer"]
+
+const typeConfig = {
+    strings: positions,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+};
+
 </script>
 
 <template>
@@ -9,7 +21,10 @@
 
     <div class="container" data-aos="fade-up" data-aos-delay="100">
     <h2>Alejandro Miralles</h2>
-    <p>Soy <span class="typed" data-typed-items="Web Developer, Full-Stack Developer, Back-End Developer, Developer">Developer</span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span></p>
+    <Typed :options="typeConfig" >
+        <p>Soy <span class="typing"></span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span></p>
+    </Typed>
+
     </div>
 
     </section><!-- /Hero Section -->
